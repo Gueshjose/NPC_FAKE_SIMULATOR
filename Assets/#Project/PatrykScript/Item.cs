0ed8cdb4dadffe6b4aa46899eece93394cbd50c4
Patryk Zajko <patryk.zajko.3@gmail.com>
2024-07-05 12:30:08 +0200

    Craftage Arme

--- Assets/#Project/PatrykScript/Item.cs
+++ Assets/#Project/PatrykScript/Item.cs
@@ -16,6 +16,7 @@ public class Item:MonoBehaviour
 
     [SerializeField]Material craftingMaterial;
     Material finalMaterial;
+    float pourcent = -0.2f;
 
     Item(string nom,ItemType type)
     {
@@ -26,5 +27,22 @@ public class Item:MonoBehaviour
     private void Start() {
         finalMaterial = transform.GetComponent<Renderer>().material;
         transform.GetComponent<Renderer>().material = craftingMaterial;
+        craftingMaterial.SetFloat("_Pourcentage",pourcent);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.tag =="hammer")
+        {
+            if(pourcent < 0.7f)
+            {
+            pourcent += 0.2f;
+            craftingMaterial.SetFloat("_Pourcentage",pourcent);
+        }else
+        {
+            transform.GetComponent<Renderer>().material = finalMaterial;
+        }
+        
+        }
     }
 }
